9825815cb31fa0accdd1ede477d982fa18b4c7a5
dentver <zirkovd4@gmail.com>
2026-07-23 19:00:25 +0300

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from 'next'
 import { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  icons: [
+    { rel: 'icon', url: '/favicon.svg', type: 'image/svg+xml' },
+    { rel: 'shortcut icon', url: '/favicon.svg', type: 'image/svg+xml' },
+  ],
+}
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return children
